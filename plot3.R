7e784c981aadb994d641fee3d84f5ce30aa9d5ad
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-13 21:57:31 +0100

    exploratory data analysis task 1

---
+++ plot3.R
@@ -0,0 +1,8 @@
+# Change the next line: current working directory should be the git directory
+setwd("~/git/ExData_Plotting1")
+# Change the next line: path to the power consumption data
+inputFile <- '~/git/exData_Plotting1/household_power_consumption.txt'
+
+source("base-plotting-assignment.R")
+loadData()
+saveplot3()
